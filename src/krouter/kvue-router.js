2b91ca1657b4fffacb608e8748381489f9f0b4d9
aibaoxiaodon <sxczhxd@163.com>
2020-12-03 21:24:05 +0800

    路由匹配

--- src/krouter/kvue-router.js
+++ src/krouter/kvue-router.js
@@ -4,18 +4,45 @@ class VueRouter {
   constructor(options) {
     this.options = options
 
-    const initCurrent = window.location.hash.slice(1) || '/'
-    Vue.util.defineReactive(this, 'current', initCurrent)
+    this.current = window.location.hash.slice(1) || '/'
+    Vue.util.defineReactive(this, 'matched', [])
+    // 递归所有的路由包括子路由
+    this.match()
 
     window.addEventListener('hashchange', this.onHashChange.bind(this))
   }
 
   onHashChange() {
     this.current = window.location.hash.slice(1)
+
+    this.matched = []
+    this.match()
+
+  }
+
+  match(routes) {
+    routes = routes || this.options.routes
+
+    // 递归遍历
+    for (const route of routes) {
+      if (route.path === '/' && this.current === '/ ') {
+        this.matched.push(route)
+        return
+      }
+
+      if (route.path !== '/' && this.current.indexOf(route.path) !== -1) {
+        this.matched.push(route)
+        if (route.children.length > 0) {
+          this.match(route)
+        }
+
+        return
+      }
+    }
   }
 }
 
-VueRouter.install = function(_Vue) {
+VueRouter.install = function (_Vue) {
   Vue = _Vue
 
   Vue.mixin({
@@ -40,10 +67,26 @@ VueRouter.install = function(_Vue) {
 
   Vue.component('router-view', {
     render(h) {
+      // 标记routerView
+      this.$vnode.data.routerView = true
+
+      let depth = 0
+      let parent = this.$parent
+
+      while (parent) {
+        const vnodeData = parent.$vnode && parent.$vnode.data
+
+        if (vnodeData) {
+          if (vnodeData.routerView) {
+            depth++
+          }
+        }
+
+        parent = parent.$parent
+      }
+
       let Component = null
-      const route = this.$router.options.routes.find(
-        (route) => route.path === this.$router.current
-      )
+      const route = this.$router.matched[depth]
       if (route) {
         Component = route.component
       }
---
+++ src/views/Info.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">
+    info page
+  </div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
